ca42fd9301541646643e3e438fd99ea8a9e772fa
Kyle Machulis <kyle@nonpolynomial.com>
2023-01-01 17:46:46 -0800

    build: Readd mermaid plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,10 @@ const config = {
       }),
     ],
   ],
-
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
