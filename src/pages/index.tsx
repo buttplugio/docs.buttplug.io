c427e24dddddf53c9eca05eb4e4307788bd1d3cf
Kyle Machulis <kyle@nonpolynomial.com>
2023-07-10 18:35:30 -0700

    chore: reduce logo size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src={logo} style={{maxWidth: 400}} />
+        <img src={logo} style={{maxWidth: 300}} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
