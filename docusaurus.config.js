e9db51bc7c212a9275d7f0070acd2f798377c3f8
Kyle Machulis <kyle@nonpolynomial.com>
2024-08-24 14:59:12 -0700

    Update to docusaurus v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,14 +1,15 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'buttplug.io',
   staticDirectories: ['examples', 'static'],
-  tagline: 'Intimate Haptics Control Standard and Library',
+  tagline: 'Buttplug: Intimate Haptics Control Standard and Library',
   url: 'https://buttplug.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
